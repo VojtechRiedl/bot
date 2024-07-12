4cca34a430fe37e2bc177b0bdf3020b5e69f974a
vojtech.riedl <vojtariedl66@gmail.com>
2024-07-12 15:50:28 +0200

    update

--- src/main/java/me/histal/Main.java
+++ src/main/java/me/histal/Main.java
@@ -3,9 +3,13 @@ package me.histal;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
     String token =  System.getenv("TOKEN");
+        System.out.println(token);
+
         JDA jda = JDABuilder.createDefault(token)
                 .build();
 
