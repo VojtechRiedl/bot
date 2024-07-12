dba07a768a83200f64b9d0bbf589efe140f3a7d8
vojtech.riedl <vojtariedl66@gmail.com>
2024-07-12 13:32:20 +0200

    update

--- src/main/java/me/histal/Main.java
+++ src/main/java/me/histal/Main.java
@@ -1,16 +1,14 @@
 package me.histal;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        /*
         JDA jda = JDABuilder.createDefault("ODQwNTE3NDU2OTQ1ODcyOTE3.GlHws8.OZ5utPX8Q_Ja1F5D9HQli9LliBMq_eVo7RbdiE\n")
                 .build();
 
         // optionally block until JDA is ready
         jda.awaitReady();
-
+        */
 
         System.out.println("Finished Building JDA!");
     }
