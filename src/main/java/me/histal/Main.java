29d7753d838b9574d05765068ce78cb226934037
vojtech.riedl <vojtariedl66@gmail.com>
2024-07-12 16:48:21 +0200

    update

--- src/main/java/me/histal/Main.java
+++ src/main/java/me/histal/Main.java
@@ -6,26 +6,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Map;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        String token =  System.getenv("TOKEN");
 
-        Logger logger = LoggerFactory.getLogger(Main.class);
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("../stack.env")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        java.util.logging.Logger.getAnonymousLogger().info("token" + token);
 
-        java.util.logging.Logger.getAnonymousLogger().info(System.getenv("PATH"));
-
-        java.util.logging.Logger.getAnonymousLogger().info(System.getenv("JAVA_HOME"));
-
-        java.util.logging.Logger.getAnonymousLogger().info(System.getenv("MAVEN_CONFIG"));
-
-        logger.error("token" + token);
-        logger.error(System.getenv("PATH"));
-        logger.error(System.getenv("JAVA_HOME"));
-        // Loop through all environment variables
+        String token = properties.getProperty("TOKEN");
 
         JDA jda = JDABuilder.createDefault(token)
                 .build();
