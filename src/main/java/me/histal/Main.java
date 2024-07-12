5ddbf36b81b2a249bf9b39cdf59a0874f64766f5
vojtech.riedl <vojtariedl66@gmail.com>
2024-07-12 15:58:19 +0200

    update

--- src/main/java/me/histal/Main.java
+++ src/main/java/me/histal/Main.java
@@ -4,17 +4,26 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.swing.*;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-    String token =  System.getenv("TOKEN");
-        System.out.println(token);
+    String token =  System.getenv("OS");
+        Map<String, String> env_var = System.getenv();
 
-        JDA jda = JDABuilder.createDefault(token)
-                .build();
+        // Loop through all environment variables
+        for (String envName : env_var.keySet()) {
+            // Print environment variable name and value to console
+            System.out.format("%s=%s", envName, env_var.get(envName));
+            System.out.println();
+        }
+    System.out.println(token);
 
-        // optionally block until JDA is ready
-        jda.awaitReady();
+    JDA jda = JDABuilder.createDefault(token)
+            .build();
+
+    // optionally block until JDA is ready
+    jda.awaitReady();
 
     }
 }
